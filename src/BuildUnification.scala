0b42d7c8f3ad3c7f1a40831754a558c57c097682
Vlad <vlad10795@gmail.com>
2016-05-18 16:00:01 +0300

    HW5 bug fix

--- src/BuildUnification.scala
+++ src/BuildUnification.scala
@@ -10,26 +10,28 @@ class BuildUnification(eqs: List[(TermExpression, TermExpression)]) {
   queue ++= eqs
   while (queue.nonEmpty) {
     val (term1, term2) = queue.dequeue
-    System.err.println(term1.toString + " = " + term2.toString)
-    (term1, term2) match {
-      case (x: TermFunc, y: TermFunc) =>
-        if (!x.name.equals(y.name)) throw new UnificationException
-        queue ++= x.args zip y.args
-      case (x: TermVar, y: TermExpression) =>
-        result.get(x.name) match {
-          case None =>
-            var updatedY = y
-            for ((p, v) <- result) updatedY = updatedY.substitute(p, v)
-            updatedY match {
-              case _:TermFunc =>
-                if (updatedY.contains(x.name)) throw new UnificationException
-                result.put(x.name, updatedY)
-              case _:TermVar =>
-                if (!updatedY.toString.equals(x.toString)) result.put(x.name, updatedY)
-            }
-          case Some(z) => if (!y.toString.equals(z.toString)) queue += Tuple2(y, z)
-        }
-      case (x: TermExpression, y: TermVar) => queue += Tuple2(y, x)
+    if (!term1.toString.equals(term2.toString)) {
+      //    System.err.println(term1.toString + " = " + term2.toString)
+      (term1, term2) match {
+        case (x: TermFunc, y: TermFunc) =>
+          if (!x.name.equals(y.name)) throw new UnificationException
+          queue ++= x.args zip y.args
+        case (x: TermVar, y: TermExpression) =>
+          result.get(x.name) match {
+            case None =>
+              var updatedY = y
+              for ((p, v) <- result) updatedY = updatedY.substitute(p, v)
+              updatedY match {
+                case _: TermFunc =>
+                  if (updatedY.contains(x.name)) throw new UnificationException
+                  result.put(x.name, updatedY)
+                case _: TermVar =>
+                  if (!updatedY.toString.equals(x.toString)) result.put(x.name, updatedY)
+              }
+            case Some(z) => if (!y.toString.equals(z.toString)) queue += Tuple2(y, z)
+          }
+        case (x: TermExpression, y: TermVar) => queue += Tuple2(y, x)
+      }
     }
   }
 }
